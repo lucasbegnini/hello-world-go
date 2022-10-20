83869189ddef0ad76cf0f0880a49d93b537672c0
Lucas Begnini <lucasbegnini@gmail.com>
2022-10-20 11:50:08 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Lucas"
+	idade := 31
+	versao := 1.1
+	fmt.Println("Olá Sr", nome, "sua idade é ", idade, "anos")
+	fmt.Println("Esse programa está na versão:", versao)
+}
